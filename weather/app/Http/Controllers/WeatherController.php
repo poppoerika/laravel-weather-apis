5bd44d067ece6499a679c476b3e73ec48998c45d
erikatharp <eem@momentohq.com>
2022-10-27 10:57:08 -0700

    chore: add instructions to run app to README; add Momento SDK example; fix code for MomentoStore cache

--- weather/app/Http/Controllers/WeatherController.php
+++ weather/app/Http/Controllers/WeatherController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Cache;
+use Momento\Auth\EnvMomentoTokenProvider;
+use Momento\Cache\SimpleCacheClient;
 
 class WeatherController extends Controller
 {
@@ -15,6 +17,8 @@ class WeatherController extends Controller
         $this->httpClient = $httpClient;
     }
 
+    // This function uses Cache Facade with momento as a cache driver.
+    // You need to update CACHE_DRIVER env variable to 'momento'
     public function city($city)
     {
         $apiKey = config("weatherapi.api_key");
@@ -22,33 +26,39 @@ class WeatherController extends Controller
         $cityWeatherInfo = "city_weather_info";
         $result = Cache::get($cityWeatherInfo);
         if (!is_null($result)) {
-            return json_decode($result->asHit()->value());
+            return $result;
         } else {
             $res = $this->httpClient->get($url);
             if ($res->getStatusCode() == 200) {
                 $json = $res->getBody();
                 // 10 minutes TTLc
                 Cache::put($cityWeatherInfo, $json, 600);
-                return json_decode($json);
+                return $json;
             }
         }
     }
 
+    // This function create a Momento client and uses it as cache.
     public function zipcode($zipcode, $countryCode)
     {
+        $authProvider = new EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN");
+        $momentoClient = new SimpleCacheClient($authProvider, 60);
+        $cacheName = "zipcode-cache";
+        $momentoClient->createCache($cacheName);
         $apiKey = config("weatherapi.api_key");
         $url = "https://api.openweathermap.org/data/2.5/weather?q={$zipcode},{$countryCode}&appid={$apiKey}";
         $zipcodeWeatherInfo = "zipcode_weather_info";
-        $result = Cache::get($zipcodeWeatherInfo);
-        if (!is_null($result)) {
-            return json_decode($result->asHit()->value());
-        } else {
+        $result = $momentoClient->get($cacheName, $zipcodeWeatherInfo);
+        if ($result->asHit()) {
+            return $result->asHit()->value();
+        }
+        elseif ($result->asMiss()) {
             $res = $this->httpClient->get($url);
             if ($res->getStatusCode() == 200) {
                 $json = $res->getBody();
                 // 10 minutes TTLc
-                Cache::put($zipcodeWeatherInfo, $json, 600);
-                return json_decode($json);
+                $momentoClient->set($zipcodeWeatherInfo, $zipcodeWeatherInfo, $json, 600);
+                return $json;
             }
         }
     }
