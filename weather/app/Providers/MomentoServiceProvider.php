2e73359eab5a22bd93fec1319bcbdfb3fbd71009
erikatharp <eem@momentohq.com>
2022-10-28 14:32:17 -0700

    chore: add MomentoServiceProvider and register momento driver in boot

---
+++ weather/app/Providers/MomentoServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Extensions\MomentoStore;
+use Illuminate\Support\ServiceProvider;
+
+class MomentoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        app('cache')->extend('momento', function($app, $config)
+        {
+            $store = new MomentoStore($config['cache_name'], $config['default_ttl']);
+            return app('cache')->repository($store);
+        });
+    }
+}
